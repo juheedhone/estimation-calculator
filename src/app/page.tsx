002ec5776bc72303da638d0152f1c768dc3b24eb
Juhee Dhone <juheedhone12@gmail.com>
2025-01-31 00:54:07 +0530

    working Form(add task)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,20 @@
 "use client";
 import { tasksFixture } from "@/fixture/tasks.fixture";
 import { useState } from "react";
-import AddTask from "../components/AddTask";
+import AddTask from "../components/FormAddTask";
 import TimeTracker from "../components/TimeTracker";
 
 const page = () => {
   const [tasks, setTasks] = useState(tasksFixture);
   return (
     <>
-      <AddTask />
+      <AddTask
+        onSubmit={(data) => {
+          console.log(data);
+          const estimate = data.estimateTimeHour * 60 + data.estimateTimeMin;
+          setTasks([...tasks, { name: data.name, estimate: estimate }]);
+        }}
+      />
       <TimeTracker tasks={tasks} />
     </>
   );
---
+++ src/components/FormAddTask.tsx
@@ -0,0 +1,115 @@
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+const schema = z.object({
+  name: z.string().min(3).max(10),
+  estimateTimeHour: z.number(),
+  estimateTimeMin: z.number().min(10),
+});
+
+type FormData = z.infer<typeof schema>;
+
+interface Props {
+  onSubmit: (data: FormData) => void;
+}
+
+const AddTask = ({ onSubmit }: Props) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({ resolver: zodResolver(schema) });
+
+  // const onSubmit = (data: FieldValues) => console.log(data);
+  return (
+    <div className="flex justify-end py-5 mr-2">
+      <Dialog>
+        <DialogTrigger asChild>
+          <Button>Add Task</Button>
+        </DialogTrigger>
+        <DialogContent className="rounded-md">
+          <DialogHeader>
+            <DialogTitle>Add-Task</DialogTitle>
+            <DialogDescription>
+              Make changes to your profile here. Click save when you're done.
+            </DialogDescription>
+          </DialogHeader>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div className="py-4  space-y-6">
+              <div className="space-y-2">
+                <Label htmlFor="name" className="text-left">
+                  Name
+                </Label>
+                <Input
+                  {...register("name")}
+                  id="name"
+                  type="text"
+                  className="col-span-3"
+                />
+                {errors.estimateTimeMin && (
+                  <p className="text-red-700">
+                    {errors.estimateTimeMin.message}
+                  </p>
+                )}
+              </div>
+              <div className="flex items-center justify-between">
+                <Label htmlFor="username" className="text-left">
+                  Time Estimation
+                </Label>
+
+                <div className="flex items-center gap-2">
+                  <Input
+                    {...register("estimateTimeHour", { valueAsNumber: true })}
+                    id="username"
+                    type="number"
+                    className="w-16"
+                  />
+                  {errors.estimateTimeHour && (
+                    <p className="text-red-700">
+                      {errors.estimateTimeHour.message}
+                    </p>
+                  )}
+                  <p className="mr-4">hour</p>
+                  <Input
+                    {...register("estimateTimeMin", { valueAsNumber: true })}
+                    id="username"
+                    type="number"
+                    className="w-16"
+                  />
+                  {errors.estimateTimeMin && (
+                    <p className="text-red-700">
+                      {errors.estimateTimeMin.message}
+                    </p>
+                  )}
+                  <p>min</p>
+                </div>
+              </div>
+            </div>
+
+            <DialogFooter>
+              <DialogClose>
+                <Button type="submit">Submit</Button>
+              </DialogClose>
+            </DialogFooter>
+          </form>
+        </DialogContent>
+      </Dialog>
+    </div>
+  );
+};
+
+export default AddTask;
