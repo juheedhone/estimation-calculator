940035912d40c462e8b20711f7bb771817690027
Juhee Dhone <juheedhone12@gmail.com>
2025-02-09 15:52:51 +0530

    Practice

---
+++ src/app/practice/page.tsx
@@ -0,0 +1,11 @@
+const page = () => {
+  return (
+    <ul className="border-2 rounded-md overflow-hidden *:py-2  border-black w-52 *:border-b-2 *:border-black *:pl-4">
+      <li className="last:border-0 hover:bg-gray-300">first</li>
+      <li className="last:border-0 hover:bg-gray-300">second</li>
+      <li className="last:border-0 hover:bg-gray-300">third</li>
+    </ul>
+  );
+};
+
+export default page;
